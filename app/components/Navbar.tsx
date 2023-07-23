d19b4deae837b40a7936711a98d6d644fe8c19a3
Santosh Singh <superssingh@gmail.com>
2023-07-24 01:32:04 +0530

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,45 @@
+"use client"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import Image from "next/image"
+
+const Navbar = () => {
+  return (
+    <div className="flex  w-full h-fit p-4 bg-slate-50 shadow-sm place-items-center font-semibold text-base">
+      <Image
+        alt="InstaApp"
+        src={"/vercel.svg"}
+        width={100}
+        height={30}
+        priority
+        className="mx-5"
+      />
+
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Post</NavigationMenuTrigger>
+            <NavigationMenuContent className="grid w-full">
+              <NavigationMenuLink className="p-3 hover:bg-black/10">
+                Add
+              </NavigationMenuLink>
+              <NavigationMenuLink className="p-3 hover:bg-black/10 ">
+                Posts
+              </NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  )
+}
+
+export default Navbar
